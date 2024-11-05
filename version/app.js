e9839005b6639c195e088c44e8ea585324d9ee1e
D. Paolella <dpaolell@redhat.com>
2024-11-05 12:31:35 +0100

    Another change

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'Testing webhooks!' + '\n';
+    response = 'All good!' + '\n';
 
     //send the response to the client
     res.send(response);
